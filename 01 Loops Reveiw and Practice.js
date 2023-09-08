5a3783cc62bcfeacb179bc45e52f9fe130f79079
stefan-scott <scottst@spsd.sk.ca>
2023-09-08 12:30:22 -0600

    Create 01 Loops Reveiw and Practice.js
    First Upload

---
+++ 01 Loops Reveiw and Practice.js
@@ -0,0 +1,65 @@
+// Loops Recap
+// Traditional For Loop
+// While Loop
+// Enhanced For    For...of
+
+//C1: Write a program that prints out all
+//    the even numbers from 1-100 using
+//    traditional FOR Loop
+
+//C2: Write a program to print out all odd
+//    numbers from 99 down to 1, using while
+
+//C3: Write a program that prints out each
+//    Name from an array 10 times in a arow
+//    (make an array with 3 names)
+
+// console.log("string to print")
+
+//#4: Nested Loops
+/*
+  let str = "Saskatoon";
+  let arr = ["apple", "orange", "pear"];
+  
+  for(let item of arr){
+    let number = 1;
+    while (number <= 5){  //repeat 5
+      number = number + 1;  //number++ number+=1
+      console.log(item);
+    }
+  }
+*/
+
+//#3: For..Of Loop:
+/*
+let str = "Saskatoon";
+let arr = ["apple", "orange", "pear"];
+for(let item of arr){
+  console.log(item);
+}
+*/
+
+
+//#2: While Loop. Print out 1, 2, .., 5
+/*
+let number = 1;
+while (number <= 5){
+  console.log(number);
+  number = number + 1;  //number++ number+=1
+}
+*/
+
+
+//#1:  Trad For Loop, count from 20 to 1
+//20 19 18 ... 3, 2, 1, STOP  i--
+/*
+for(let i=20; i>=1 ; i=i-1){
+  console.log(i);
+}
+*/
+
+// Data Types
+// Boolean, Strings, Numbers
+// special:  unknown 
+// NaN  Not a Numbers
+// Infinity  -Infinity
