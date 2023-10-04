788a61ed992535987b9a62f80ba9a344e10ca23f
stefan-scott <scottst@spsd.sk.ca>
2023-10-04 12:04:07 -0600

    finished demo

--- 06 Noisy Demo/sketch.js
+++ 06 Noisy Demo/sketch.js
@@ -8,12 +8,17 @@
 //Global Variables
 let segmentLength = 5;
 let ballY = 200;
+let noisePositionRound = 50; //abritrary starting spot
+let noisePositionLine = 200;
+let noisePositionBallY = 0;
 
+
+const noiseShift = 0.2; //the larger this value, the more
+                 // unrelated the noise results are
 function setup() {
   createCanvas(500, 500);
   frameRate(10);
 }
-
 function wastefulLine(){
   // using a loop, let's draw a horizontal line made up of little segments
   let x = 0;
@@ -21,6 +26,11 @@ function wastefulLine(){
   while(x < width){
     //option 1 - using random()
     let lineValue = random(0,255);
+
+    //option 2 - using noise()
+    lineValue = noise(noisePositionLine); //0-1
+    lineValue = map(lineValue,0,1,0,255);
+    noisePositionLine += noiseShift;
 
     stroke(lineValue);
     line(x, height/2, x+segmentLength, height/2);
@@ -37,13 +47,24 @@ function rectangleOnLine(){
   //option 1 - using random() [uniform distribution]
   let roundedAmount = random(2,70);
 
+  //option 2 - using noise()
+  roundedAmount = noise(noisePositionRound); //0-1
+  roundedAmount = map(roundedAmount,0,1,2,70);
+  noisePositionRound += noiseShift;
+
   rect(width * 0.2, height/2, width * 0.5, height * 0.2, roundedAmount);
 }
 
 function moveBall(){
   //option 1 - random()
-  ballY += random(-20,20);
-  
+  //ballY += random(-20,20);
+
+  //option 2 - noise()
+  let dY = noise(noisePositionBallY); //0-1
+  dY = map(dY,0,1,-20,20);
+  noisePositionBallY += noiseShift;
+  ballY += dY;
+
   circle(width * 0.75, ballY, 30);
 }
 
