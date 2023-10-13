98be112041b49e4df405f6bd2897c8df0ba44bb0
stefan-scott <scottst@spsd.sk.ca>
2023-10-13 12:07:59 -0600

    nearly done

--- 10 Objects Demo Two/sketch.js
+++ 10 Objects Demo Two/sketch.js
@@ -5,6 +5,7 @@
 
 // Global Variables
 let points = [];
+let reach = 100;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -12,44 +13,64 @@ function setup() {
 
 function draw() {
   background(255);
-  for(let p of points){
+  for (let p of points) {
     p.move();
     p.display();
+    p.connectPoints(points);
   }
 
 }
 
-function mouseClicked(){
-  points.push(new MiniPoint(mouseX,mouseY));
+function mouseClicked() {
+  points.push(new MiniPoint(mouseX, mouseY));
 }
 
-class MiniPoint{
+class MiniPoint {
   //constructor function
-  constructor(x,y){  //set up class variables
-    this.x = x;    this.y = y;    this.s = 20;
-    this.c = color(random(255),random(255),random(255));
-    this.xTime = random(10);    this.yTime = random(10);
-    this.timeShift = 0.01;   this.maxSpeed = 5; 
+  constructor(x, y) {  //set up class variables
+    this.x = x; this.y = y; this.s = 20;
+    this.c = color(random(255), random(255), random(255));
+    this.xTime = random(10); this.yTime = random(10);
+    this.timeShift = 0.01; this.maxSpeed = 5;
   }
 
   //class functions
-  move(){
-    let xSpeed = noise(this.xTime); //0-1    //-5 to 5
-    xSpeed = map(xSpeed,0,1,-this.maxSpeed, this.maxSpeed);
-    let ySpeed = noise(this.yTime);
-    ySpeed = map(ySpeed,0,1,-this.maxSpeed,this.maxSpeed);
-    this.x += xSpeed;   this.y += ySpeed;
-    this.xTime += this.timeShift;    
-    this.yTime+=this.timeShift;
-
-    //Wrap around code 
-    if(this.x < 0) this.x += width;
-    if(this.x > width) this.x -= width;
-    if(this.y < 0) this.y += height;
-    if(this.y > height) this.y -= height;
+  connectPoints(pointArray) {
+    //take an array, and connect this object to any 
+    //nearby MiniPoints with a line segment
+    stroke(this.c);
+    for (let p of points) {
+      //p - the object we are comparing to
+      //this.x  this.y  p.getX()  p.getY()
+      if (this !== p) { //check that p isn't myself
+        if (dist(this.x, this.y, p.getX(), p.getY()) < reach) {
+          line(this.x, this.y, p.getX(), p.getY());
+        }
+      }
+    }
   }
 
-  display(){
+  getX() { return this.x }  //getter methods
+  getY() { return this.y }
+
+
+  move() {
+    let xSpeed = noise(this.xTime); //0-1    //-5 to 5
+    xSpeed = map(xSpeed, 0, 1, -this.maxSpeed, this.maxSpeed);
+    let ySpeed = noise(this.yTime);
+    ySpeed = map(ySpeed, 0, 1, -this.maxSpeed, this.maxSpeed);
+    this.x += xSpeed; this.y += ySpeed;
+    this.xTime += this.timeShift;
+    this.yTime += this.timeShift;
+
+    //Wrap around code 
+    if (this.x < 0) this.x += width;
+    if (this.x > width) this.x -= width;
+    if (this.y < 0) this.y += height;
+    if (this.y > height) this.y -= height;
+  }
+
+  display() {
     fill(this.c);
     noStroke();
     circle(this.x, this.y, this.s);
