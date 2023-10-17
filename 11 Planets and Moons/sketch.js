33ed9abdccb29c6639f533669a7a98b50c5402aa
stefan-scott <scottst@spsd.sk.ca>
2023-10-17 11:26:46 -0600

    wip2

--- 11 Planets and Moons/sketch.js
+++ 11 Planets and Moons/sketch.js
@@ -1,15 +1,30 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
-
+// Planets and Moons
+// Mr. Scott
+// October 17, 2023
+// A look at re-creating objects, objects IN objects
+let myPlanet;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  myPlanet = new Planet(width/2, height/2);
 }
 
 function draw() {
   background(220);
+  myPlanet.display();
+}
+
+class Planet{
+  constructor(x,y){
+    this.x = x;     this.y = y;
+    this.s = 100;   this.radius = this.s/2;
+    this.moons = [];  //fill with moon objects
+  }
+  display(){
+    circle(this.x, this.y, this.s);
+  }
+}
+
+class Moon{
+
 }
