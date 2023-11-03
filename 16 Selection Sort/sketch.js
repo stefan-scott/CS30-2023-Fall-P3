2737f7faa00a765d4438c7b338b4acb5f049d2d6
stefan-scott <scottst@spsd.sk.ca>
2023-11-03 13:32:13 -0600

    completed

--- 16 Selection Sort/sketch.js
+++ 16 Selection Sort/sketch.js
@@ -7,8 +7,28 @@ let values = [];
 const ARRAY_SIZE = 20;
 
 function setup() {
-  noCanvas();  populateArray();
-  print(values)
+  noCanvas();    populateArray();   print(values);
+  selectionSort();    print(values);
+}
+
+function selectionSort(){
+  //for each index, find smallest remaining value to the right
+  //and swap if it is lower than the item at the index.
+  for(let index = 0; index < values.length-1; index++){
+    let minimum = values[index];
+    let minimumLoc = index;
+    for(let searchIndex = index+1; searchIndex < values.length; searchIndex++){
+      let cur = values[searchIndex];
+      if (cur < minimum){
+        minimum = cur;
+        minimumLoc = searchIndex;
+      }
+    }
+    //swap the item at index with the item at minimumLoc
+    let temp = values[index];
+    values[index] = values[minimumLoc];
+    values[minimumLoc] = temp;
+  }
 }
 
 function populateArray(){
