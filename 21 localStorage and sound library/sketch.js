cc2d706f3b8fa4bef146bd1fdf89480c9e9cc1b4
stefan-scott <scottst@spsd.sk.ca>
2023-11-27 11:38:48 -0600

    WIP

--- 21 localStorage and sound library/sketch.js
+++ 21 localStorage and sound library/sketch.js
@@ -27,5 +27,14 @@ class Ball{ //a ball that bounces on the canvas edges
   }
   move(){
     this.pos.add(this.vel);
+    //check for wall collisions
+    if(this.pos.x<0 || this.pos.x>width){
+      this.vel.x *= -1;
+      totalBounces += 1;
+    }
+    if(this.pos.y<0 || this.pos.y>height){
+      this.vel.y *= -1;
+      totalBounces +=1;
+    }
   }
 }
