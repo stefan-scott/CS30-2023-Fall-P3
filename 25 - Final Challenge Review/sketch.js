48b951822da49eef9410e9d1902e53e9409302bf
Scott <ScottSt@spsd.sk.ca>
2024-01-17 12:13:09 -0600

    Finished the Demo

--- 25 - Final Challenge Review/sketch.js
+++ 25 - Final Challenge Review/sketch.js
@@ -2,11 +2,24 @@
 
 let gorillaIdle = [];
 let gorillaSwipe = [];
+let spiralImages = [];
+
+let spirals = []; //spiral objects
 
 let gorillaX, gorillaY;
 let idleIndex = 0; let swipeIndex = 0;
 
 function preload() { //to ensure all images are loaded
+  //SPIRALS  00 - 09   10- 15
+  for (let i = 0; i < 16; i++) {
+    if (i < 10) {
+      spiralImages.push(loadImage("assets/Circle/Circle Animation0" + i + ".png"));
+    }
+    else {
+      spiralImages.push(loadImage("assets/Circle/Circle Animation" + i + ".png"));
+    }
+  }
+
   //IDLE   idle1.png  →6
   for (let i = 1; i < 7; i++) {
     gorillaIdle.push(loadImage("assets/Gorilla/idle" + i + ".png"));
@@ -26,7 +39,7 @@ function setup() {
 function draw() {
   background(220);
   if (keyIsPressed && key === " ") {  //SWIPE
-    image(gorillaSwipe[swipeIndex], gorillaX, gorillaY);
+    image(gorillaSwipe[swipeIndex], gorillaX, gorillaY, 300, 300);
     if (frameCount % 4 === 0) {
       swipeIndex++;
       if (swipeIndex > 5) swipeIndex = 0;
@@ -38,7 +51,40 @@ function draw() {
       idleIndex++;
       if (idleIndex > 5) idleIndex = 0;
     }
+  }
+  //SPIRALS  []  i==3
+  for (let i = 0; i < spirals.length; i++) {
+    let s = spirals[i]; //current spiral
+    s.display();
+    if (s.active === false) {
+      spirals.splice(i,1);
+      i--; //needed if looping left→right
+    }
+  }
+}
 
+  function mousePressed() {
+    spirals.push(new Spiral(mouseX, mouseY));
   }
 
-}
+
+  class Spiral {
+    constructor(x, y) {
+      this.pos = createVector(x, y);
+      this.currentFrame = 0; //0-15
+      this.active = true; //for deletion
+    }
+    display() {
+      if (this.currentFrame > 15) {
+        this.active = false;
+      }
+      else {
+        image(spiralImages[this.currentFrame], this.pos.x, this.pos.y);
+        if (frameCount % 4 === 0) {
+          this.currentFrame++;
+        }
+      }
+    }
+
+
+  }
